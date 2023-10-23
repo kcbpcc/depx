8a3dd9288151e338dba87d410f549840b4c4c036
PreciseXceleratedYield <148702748+PreciseXceleratedYieldPXY@users.noreply.github.com>
2023-10-23 19:59:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kiteext',
+      version='1.0',
+      description='a fork of kiteext',
+      author='b karthick',
+      author_email='prog@ecomsense.in',
+      url='https://github.com/pannet1/kiteext/',
+      packages=['kiteext']
+      )
